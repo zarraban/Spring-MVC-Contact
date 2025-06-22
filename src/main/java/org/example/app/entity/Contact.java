32befcd9526a817ef2d6620e492d30dbe7d6e75a
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-22 14:38:38 +0300

    Add Contact entity

---
+++ src/main/java/org/example/app/entity/Contact.java
@@ -0,0 +1,28 @@
+package org.example.app.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name ="contacts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private String phone;
+
+}
