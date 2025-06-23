78ff3b4312416f3dab8682ea049690a98b65192f
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-23 15:52:53 +0300

    Add dependencies and cfg for logback

--- src/main/java/org/example/app/service/contact/ContactServiceImpl.java
+++ src/main/java/org/example/app/service/contact/ContactServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.app.service.contact;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.app.dto.RequestContact;
 import org.example.app.entity.Contact;
 import org.example.app.repository.contact.ContactRepository;
@@ -12,6 +13,7 @@ import java.util.Optional;
 
 
 @Service("contactService")
+@Slf4j
 public class ContactServiceImpl implements ContactService {
 
 
@@ -26,35 +28,41 @@ public class ContactServiceImpl implements ContactService {
     @Transactional
     @Override
     public void create(RequestContact request) {
+        log.info("Service started creating contact");
         repository.create(request);
     }
     @Transactional(readOnly = true)
     @Override
     public List<Contact> readAll() {
+        log.info("Service started readingAll contacts");
         return repository.readAll().orElse(null);
     }
 
     @Transactional(readOnly = true)
     @Override
     public Contact readById(Long id) {
+        log.info("Service started readingById contact with id = {}",id);
         return repository.readById(id).orElse(null);
     }
 
     @Transactional
     @Override
     public boolean updateById(Long id, RequestContact request) {
+        log.info("Service started updating contact with id = {}",id);
         return repository.updateById(id,request);
     }
 
     @Transactional
     @Override
     public boolean deleteById(Long id) {
+        log.info("Service started deleting contact with id = {}",id);
         return repository.deleteById(id);
     }
 
     @Transactional(readOnly = true)
     @Override
     public Contact readLast() {
+        log.info("Service started readingLast contact");
         return repository.readLast().orElse(null);
     }
 }
