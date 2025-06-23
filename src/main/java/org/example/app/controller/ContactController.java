6969dfcd22db6756bde45f61c99c9cf683965057
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-23 15:08:36 +0300

    Add controller for Contact

---
+++ src/main/java/org/example/app/controller/ContactController.java
@@ -0,0 +1,77 @@
+package org.example.app.controller;
+
+import org.example.app.dto.RequestContact;
+import org.example.app.entity.Contact;
+import org.example.app.service.contact.ContactService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+@Controller
+public class ContactController {
+
+    private final ContactService service;
+
+    public ContactController(@Qualifier("contactService")ContactService service){
+        this.service = service;
+    }
+
+    @GetMapping("/contacts")
+    public String fetchAllContacts(Model model){
+        List<Contact> list = service.readAll();
+        model.addAttribute("title", "Contacts");
+        if(list.isEmpty()){
+            model.addAttribute("contactInfo", "No contacts added yet :(");
+        }else {
+            model.addAttribute("contacts", list);
+
+        }
+        model.addAttribute("fragmentName","contact-list");
+
+        return "layout";
+    }
+
+    @GetMapping("/create-contact")
+    public String createContact(Model model){
+        model.addAttribute("title", "Add contact");
+        model.addAttribute("fragmentName","contact-add");
+
+        return "layout";
+    }
+
+    @PostMapping("/add-contact")
+    public RedirectView addUser(@ModelAttribute RequestContact requestContact){
+        RedirectView redirectView = new RedirectView("./contacts");
+        service.create(requestContact);
+        return redirectView;
+
+    }
+
+    @RequestMapping("/update-contact/{id}")
+    public String updateContact(@PathVariable("id")Long id, Model model){
+        model.addAttribute("title", "Update contact");
+        Contact contact = service.readById(id);
+        model.addAttribute("contact", contact);
+        model.addAttribute("fragmentName", "contact-update");
+        return "layout";
+    }
+
+    @PostMapping("/change-contact")
+    public RedirectView changeContact(@ModelAttribute RequestContact requestContact){
+        RedirectView redirectView = new RedirectView("./contacts");
+        service.updateById(requestContact.id(),requestContact);
+        return redirectView;
+
+    }
+    @GetMapping("/delete-contact/{id}")
+    public RedirectView deleteContact(@PathVariable("id")Long id){
+        RedirectView redirectView = new RedirectView("../contacts");
+        service.deleteById(id);
+        return redirectView;
+    }
+
+}
