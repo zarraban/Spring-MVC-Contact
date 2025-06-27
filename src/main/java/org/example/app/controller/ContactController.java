89e9c8571ce594987fb05a1c9d1c96b94d3b0aa2
Smyrnov Vlad <vladsimovv@gmail.com>
2025-06-27 10:51:46 +0300

    Update /contacts endpoint with AIgreet field

--- src/main/java/org/example/app/controller/ContactController.java
+++ src/main/java/org/example/app/controller/ContactController.java
@@ -2,6 +2,7 @@ package org.example.app.controller;
 
 import org.example.app.dto.RequestContact;
 import org.example.app.entity.Contact;
+import org.example.app.service.ai.AiService;
 import org.example.app.service.contact.ContactService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -15,14 +16,40 @@ import java.util.List;
 public class ContactController {
 
     private final ContactService service;
+    private final AiService aiService;
 
-    public ContactController(@Qualifier("contactService")ContactService service){
+    public ContactController(@Qualifier("contactService")ContactService service, @Qualifier("aiService") AiService aiService){
         this.service = service;
+        this.aiService = aiService;
     }
 
     @GetMapping("/contacts")
     public String fetchAllContacts(Model model){
         List<Contact> list = service.readAll();
+        model.addAttribute("title", "Contacts");
+        if(list.isEmpty()){
+            model.addAttribute("contactInfo", "No contacts added yet :(");
+        }else {
+            model.addAttribute("contacts", list);
+
+        }
+        model.addAttribute("fragmentName","contact-list");
+
+        return "layout";
+    }
+
+    @PostMapping("/contacts")
+    public String fetchAllContactsAndGetName(@RequestParam("greetName") String name, Model model){
+        List<Contact> list = service.readAll();
+        if(name == null || name.trim().isEmpty()){
+            model.addAttribute("greetText","Please enter name!");
+        }
+        else {
+            String aiResponse = aiService.callAiWithPrompt("generate short greeting(about 10< words) for the guy or girl with name " + name.trim()+
+                    " write only one greeting and nothing else. But this greeting should be every time unique");
+            model.addAttribute("greetText",aiResponse);
+        }
+
         model.addAttribute("title", "Contacts");
         if(list.isEmpty()){
             model.addAttribute("contactInfo", "No contacts added yet :(");
